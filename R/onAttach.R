302dad3514fae833e3074dff3c0b36dfbdf0f7f4
Jorge Castillo-Mateo <jorgecastillomateo@gmail.com>
2020-07-14 16:45:34 +0200

    Adding...

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   
-  packageStartupMessage('Welcome to RecordTest \U1F4AA'# \n 
-    #Learn more about RecordTest in vignette("RecordTest")'
-                        )
+  packageStartupMessage("Welcome to RecordTest \U1F4AA")
 }
