332226564d9db6c53718a7b470fe594772e2d57e
Jorge Castillo-Mateo <jorgecastillomateo@gmail.com>
2021-08-08 17:56:40 +0200

    VERSION 2.1.0

--- R/ZaragozaSeries-data.R
+++ R/ZaragozaSeries-data.R
@@ -1,5 +1,5 @@
 #' @title Splitted and Uncorrelated Time Series \code{\link{TX_Zaragoza}}
-#' @description The matrix resulting from the data-preparation (or 
+#' @description The matrix resulting from the data preparation (or 
 #'   pre-processing) of \code{\link{TX_Zaragoza}$TX}. 
 #' 
 #' @details 
@@ -9,8 +9,8 @@
 #'   The data matrix corresponds to the 70 years with observations in 
 #'   \code{\link{TX_Zaragoza}$TX} and to the 76 days in the year where adjacent
 #'   daily maximum temperature subseries are uncorrelated. By coincidence, 
-#'   none of the subseries 4, 90 or 278 with imputed values is kept within the
-#'   76 uncorrelated days.
+#'   none of the subseries 4, 90 or 278 with missing observations is kept 
+#'   within the 76 uncorrelated days.
 #'
 #' @docType data
 #' @keywords datasets
